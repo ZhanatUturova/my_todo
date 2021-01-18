37afde8a687b9fc7973289925623de6045d28061
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-01-18 16:10:33 +0600

    set static and media urls and roots

--- todo/settings.py
+++ todo/settings.py
@@ -120,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_R00T = BASE_DIR / 'media'
