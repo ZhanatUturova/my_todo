7b373d61bf28a0592b7dac27b29c92ea97ce0a31
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-01-25 08:37:20 +0600

    added todo closing

--- main/views.py
+++ main/views.py
@@ -27,6 +27,12 @@ def mark_todo(request, id):
     todo.save()
     return redirect(test)
 
+def close_todo(request, id):
+    todo = ToDo.objects.get(id=id)
+    todo.is_closed = not todo.is_closed
+    todo.save()
+    return redirect(test)
+
 def go(request):
     return render(request, 'go.html')
 
