7a93d9d6dd1081185b355d78462f2f72408e9910
ZhanatUturova <63417019+ZhanatUturova@users.noreply.github.com>
2021-01-25 12:07:33 +0600

    corected models

--- main/models.py
+++ main/models.py
@@ -11,10 +11,10 @@ class ToDo(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=150)
     subtitle = models.CharField(max_length=250)
-    description = models.CharField(max_length=500)
-    price = models.CharField(max_length=10)
+    description = models.TextField(default=0)
+    price = models.IntegerField(default=0)
     genre = models.CharField(max_length=100)
     autor = models.CharField(max_length=35)
-    year = models.CharField(max_length=4)
+    year = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     is_favorite = models.BooleanField(default=False)
